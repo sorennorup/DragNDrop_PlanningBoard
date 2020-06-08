bf1885d2ad40d0ab85f6794c5bded45244e33c75
Søren Nørup <soren.norup@uudanmark.dk>
2020-06-08 12:00:02 +0200

    adds pdfmake

--- scripts.js
+++ scripts.js
@@ -1,66 +1,40 @@
 $('document').ready(init);
 		
-        function init(){
-            var itemArray = ['item1', 'item2','item3'];
-            var itemArray3 = ['item4', 'item5'];
-            var itemArray2 = ['item6', 'item7','item8'];
+    function init(){
+        var itemArray = ['item1', 'item2','item3'];
+        var itemArray3 = ['item4', 'item5'];
+        var itemArray2 = ['item6', 'item7','item8'];
+        var allItems = '#item1, #item2,#item3,#item4,#item5,#item6,#item7,#item8';
+        
+        setItemsOnDragStart(allItems);   
+        createDraggableArea('#done1',itemArray);
+        createDraggableArea('#done2',itemArray2);
+        createDraggableArea('#done3', itemArray3);
 
-            $('#item1, #item2,#item3,#item4,#item5,#item6,#item7,#item8').bind('dragstart', function(event) {
-                event.originalEvent.dataTransfer.setData("text/plain", event.target.getAttribute('id'));
-            });
-            
-  		    // bind the dragover event on the board sections
-            $('#done1').bind('dragover', function(event) {
-                event.preventDefault();
-            });
-			// bind the drop event on the board sections
-  			$('#done1').bind('drop', function(event) {
-            var notecard = event.originalEvent.dataTransfer.getData("text/plain");
-            window.sessionStorage.setItem(notecard,$('#'+notecard).text());
-            for(var i = 0; i < itemArray.length; i++){
-               if(itemArray[i] == notecard)
-               event.target.appendChild(document.getElementById(notecard));
-            }
-			// Turn off the default behaviour
-			// without this, FF will try and go to a URL with your id's name
-            event.preventDefault();
-			});
 
+        function createDraggableArea(areaid, items){
              // bind the dragover event on the board sections
-             $('#done2').bind('dragover', function(event) {
+            $(areaid).bind('dragover', function(event) {
                 event.preventDefault();
             });
 			// bind the drop event on the board sections
-  			$('#done2').bind('drop', function(event) {
+  			$(areaid).bind('drop', function(event) {
             var notecard = event.originalEvent.dataTransfer.getData("text/plain");
             window.sessionStorage.setItem(notecard,$('#'+notecard).text());
-            for(var i = 0; i < itemArray.length; i++){
-               if(itemArray2[i] == notecard)
+            
+            for(var i = 0; i < items.length; i++){
+               if(items[i] == notecard)
                event.target.appendChild(document.getElementById(notecard));
             }
 			// Turn off the default behaviour
 			// without this, FF will try and go to a URL with your id's name
             event.preventDefault();
 			});
-
-            // bind the dragover event on the board sections
-            $('#done3').bind('dragover', function(event) {
-                event.preventDefault();
-            });
-			// bind the drop event on the board sections
-  			$('#done3').bind('drop', function(event) {
-            var notecard = event.originalEvent.dataTransfer.getData("text/plain");
-            window.sessionStorage.setItem(notecard,$('#'+notecard).text());
-           
-            for(var i = 0; i < itemArray.length; i++){
-               if(itemArray3[i] == notecard)
-               event.target.appendChild(document.getElementById(notecard));
-              
-              
-               
             }
-			// Turn off the default behaviour
-			// without this, FF will try and go to a URL with your id's name
-            event.preventDefault();
-			});
+        function setItemsOnDragStart(items) {
+            
+            $(items).bind('dragstart', function(event) {
+                event.originalEvent.dataTransfer.setData("text/plain", event.target.getAttribute('id'));
+        });
+        }
         }
