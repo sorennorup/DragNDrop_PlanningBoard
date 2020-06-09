8efe486068f1b9c4f66db0087bee58515bd3b93b
Søren Nørup <soren.norup@uudanmark.dk>
2020-06-09 13:34:00 +0200

    adds datamodel for the app

---
+++ data.js
@@ -0,0 +1,55 @@
+data = {
+    "block1":
+     {
+        
+        "id1": {
+            "dragid": "item1",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        },
+        "id2": {
+            "dragid": "item2",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        },
+        "id3": {
+            "dragid": "item3",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        }
+
+    },
+
+    "block2":
+     { 
+        "id1": {
+            "dragid": "item1",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        },
+        "id2": {
+            "dragid": "item2",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        },
+        "id3": {
+            "dragid": "item3",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        }
+
+    },
+
+    "block3":
+    {
+        
+        "id1": {
+            "dragid": "item1",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        },
+        "id2": {
+            "dragid": "item2",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        },
+        "id3": {
+            "dragid": "item3",
+            "value": "Opsøge unge, målgruppevurdere, afklare ungeprofiler	"
+        }
+
+    }
+
+}
